04f85ba028d407a473e469172f89bc946586b3fe
mac <mac@192.168.0.105>
2022-03-11 08:35:50 +0700

    cccc

--- HelloWorld/src/limitTimePoint.java
+++ HelloWorld/src/limitTimePoint.java
@@ -3,6 +3,10 @@ public class limitTimePoint {
 	private String namePoint;
 	private Long numberPoint;
 	private String dateLimitPoint;
+<<<<<<< master
 	private String b;
 	private String D;
+=======
+	
+>>>>>>> 82e2838 commit 2
 }
